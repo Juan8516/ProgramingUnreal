8cd2b8a5a6d5e079c779522f29841da5db7512a7
Juan8516 <juan.ind85@gmail.com>
2024-08-21 21:38:21 -0500

    feat: practica operaciones numericas -Usuario ingresa valores. -Usuario selecciona operacion a realizar (suma, resta, multiplicacion, division) -Usuario puede seleccionar cuando salir.

--- CUnreal2/CUnreal2/CUnreal2.cpp
+++ CUnreal2/CUnreal2/CUnreal2.cpp
@@ -37,19 +37,79 @@ using namespace std;
 //	system("pause");
 //}
 
-void imprimirNumeros(float num) {
-	cout << num << endl;
-}
+//void imprimirNumeros(float num) {
+//	cout << num << endl;
+//}
+//
+////2.Ciclo While
+//int main() {
+//
+//	float num1 = 0.00f;
+//	float num2 = 1.00f;
+//
+//	while (num2 >= num1)
+//	{
+//		num1 += 0.01f;
+//		imprimirNumeros(num1);
+//	}
+//}
 
-//2.Ciclo While
+
+//3.Practica
+
+//Variables
 int main() {
 
-	float num1 = 0.00f;
-	float num2 = 1.00f;
+	//Ingreso valores por consola.
+    int choice;
+    bool keepRunning = true;
 
-	while (num2 >= num1)
-	{
-		num1 += 0.01f;
-		imprimirNumeros(num1);
-	}
+    while (keepRunning) {
+        int num1, num2;
+        std::cout << "Ingrese el primer número: ";
+        std::cin >> num1;
+
+        std::cout << "Ingrese el segundo número: ";
+        std::cin >> num2;
+
+        std::cout << "\nSeleccione una operación:\n";
+        std::cout << "1. Suma\n";
+        std::cout << "2. Resta\n";
+        std::cout << "3. Multiplicación\n";
+        std::cout << "4. División\n";
+        std::cout << "5. Salir\n";
+        std::cout << "Opción: ";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            std::cout << "Resultado de la suma: " << num1 + num2 << std::endl;
+            break;
+        case 2:
+            std::cout << "Resultado de la resta: " << num1 - num2 << std::endl;
+            break;
+        case 3:
+            std::cout << "Resultado de la multiplicación: " << num1 * num2 << std::endl;
+            break;
+        case 4:
+            if (num2 != 0) {
+                std::cout << "Resultado de la división: " << num1 / num2 << std::endl;
+            }
+            else {
+                std::cout << "Error: División por cero no permitida." << std::endl;
+            }
+            break;
+        case 5:
+            keepRunning = false;
+            break;
+        default:
+            std::cout << "Opción no válida. Inténtalo de nuevo." << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << "Gracias por usar el programa. ¡Adiós!" << std::endl;
+
+    return 0;
 }
